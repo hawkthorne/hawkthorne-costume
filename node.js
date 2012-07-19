f277b4d33f1fce5a8158fefd57dd02805799d090
Jordan Hoff <jhoff@linkedin.com>
2012-07-19 17:37:03 -0500

    More heroku debugging

--- node.js
+++ node.js
@@ -50,6 +50,8 @@ app.get('/big/:path', function (req, res, next) {
 					this.stream(function (err, stdout, stderr) {
 						if (err) {
 							res.json(err,404);
+						} else if (stderr) {
+							res.json(stderr,404);
 						} else {
 							res.setHeader('Expires', new Date(Date.now() + 604800000));
 							stdout.pipe( res );
