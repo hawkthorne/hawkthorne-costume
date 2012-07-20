d770509fa9d2660d184ae6e0ed59ac85c8ada2b7
Jordan Hoff <jhoff@linkedin.com>
2012-07-19 22:54:18 -0500

    Heroku debugging

--- node.js
+++ node.js
@@ -57,14 +57,16 @@ app.get('/big/:path', function (req, res, next) {
 					if( err ) {
 						res.json(err,404);
 					} else {
-						this.scale( value.width * 5, value.height * 5 );
+						this.scale( value.width * 10, value.height * 10 );
 						this.stream(function (err, stdout, stderr) {
+							stderr.on('data',function(data){console.log('stderr',data)});
 							if (err) {
 								res.json(err,404);
 							} else {
 							    var writeStream = fs.createWriteStream( cached_file );
 								stdout.pipe( writeStream );
 								stdout.on('end', function() {
+									console.log(stdout);
 									console.log(' writing ' + cached_file + ' to cache ');
 									res.redirect( '/' + cached_file );
 								});
