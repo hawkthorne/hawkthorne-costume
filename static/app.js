9c48cfa4d02fb1d1cf716a920ba7e1182fe1c4d6
Jordan Hoff <jhoff@linkedin.com>
2012-07-20 08:12:33 -0500

    Firefox bugfix ( mouseover not working properly )

--- static/app.js
+++ static/app.js
@@ -158,11 +158,12 @@ $(document).ready(function() {
 
 function get_square_coords( e ) {
 	// firefox fix
-	var x = e.hasOwnProperty('offsetX') ? e.offsetX : e.layerX;
-	var y = e.hasOwnProperty('offsetY') ? e.offsetY : e.layerY;
+	var oe = e.originalEvent,
+		x = ( oe.offsetX == undefined ? oe.layerX : oe.offsetX ),
+		y = ( oe.offsetY == undefined ? oe.layerY : oe.offsetY );
 	return {
-		Y: Math.floor(y / 48),
-		X: Math.floor(x / 48)
+		Y: Math.floor( y / 48 ),
+		X: Math.floor( x / 48 )
 	};
 }
 
